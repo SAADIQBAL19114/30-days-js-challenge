27b8feb639b5d54465cbdd5d9286d21ec93789d1
saadiqbal19114 <saadiqbal19114@gmail.com>
2024-07-21 23:33:16 +0500

    Day 2 challenge completed

---
+++ Day:2 Operators/index.js
@@ -0,0 +1,110 @@
+// Activity 1 
+
+// Task 1
+
+let add =  4 + 5
+console.log(add)
+
+// Task 2
+
+let sub =  4 - 5
+console.log(sub)
+
+// Task 3
+
+let mul =  4 * 5
+console.log(mul)
+
+// Task 4
+
+let div =  4 / 5
+console.log(div)
+
+// Task 5
+
+let rem =  4 % 6
+console.log(rem)
+
+
+// Activity 2 
+
+
+// Task 6
+
+let number = 6
+number +=  4
+console.log(number)
+
+// Task 7
+
+let number1 = 6
+number1 -=  4
+console.log(number1)
+
+// Activity 3
+
+// Task 8
+
+let greater = 3 > 2
+console.log(greater)
+
+let less = 2 < 3
+console.log(less)
+
+// Task 9 
+
+let greaterEqual = 3 >= 3
+console.log(greaterEqual)
+
+let lessEqual = 2 >= 2 
+console.log(lessEqual)
+
+// Task 10 
+
+let equal = 2 == 2
+console.log(equal)
+
+let strictEqual = 2 === 2
+console.log(strictEqual) 
+
+// Activity 4
+
+// Task 11
+
+let isSunny = true;
+let isWarm = true;
+
+if (isSunny && isWarm) {
+  console.log("It's a great day for a walk!");
+} else {
+  console.log("The weather is not ideal for a walk.");
+}
+
+// Task 12
+
+let hasUmbrella = true;
+let isRaining = false;
+
+if (hasUmbrella || isRaining) {
+  console.log("You are prepared for the rain.");
+} else {
+  console.log("You might get wet if it rains.");
+}
+
+// Task 13
+
+let isHungry = false;
+
+if (!isHungry) {
+  console.log("You are not hungry.");
+} else {
+  console.log("You are hungry.");
+}
+
+// Activity 5
+
+// Task 14
+
+let number3 = 12
+let result = number3 > 0 ? "positive" : "negative"
+console.log(result)
